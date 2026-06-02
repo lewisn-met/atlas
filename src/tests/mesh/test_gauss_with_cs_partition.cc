f7866c3ca741f60a26c11bfde4998018af7a9da8
lewisn-met <lewis.napper@metoffice.gov.uk>
2026-06-02 11:33:48 +0100

    Added tests for different MPI ranks and grids

--- src/tests/mesh/test_gauss_with_cs_partition.cc
+++ src/tests/mesh/test_gauss_with_cs_partition.cc
@@ -16,21 +16,42 @@ namespace test {
 
 // ------------------------------------------------------------------
 
-CASE("fails on non rubix cube") {
-    const Grid grid = Grid("CS-LFR-15");
-    const util::Config meshConfig =
-        util::Config("partitioner", "cubedsphere") | util::Config("halo", 1);
+void testFunction(std::string csGridName, std::string gaussGridName) {
+    const Grid grid               = Grid(csGridName);
+    const util::Config meshConfig = util::Config("partitioner", "cubedsphere") | util::Config("halo", 1);
     const MeshGenerator meshGen   = MeshGenerator("cubedsphere", meshConfig);
-    const Mesh mesh = meshGen.generate(grid);
+    const Mesh mesh               = meshGen.generate(grid);
     const functionspace::NodeColumns CSFunctionSpace = functionspace::NodeColumns(mesh);
 
-    const StructuredGrid gaussGrid("F15");
+    const StructuredGrid gaussGrid(gaussGridName);
 
-    atlas::StructuredMeshGenerator meshGenerator;
-    const grid::MatchingMeshPartitioner CSPartitioner =
-        grid::MatchingMeshPartitioner(CSFunctionSpace.mesh(), atlas::option::type("cubedsphere"));
-    const grid::Distribution gaussToCSDistribution = CSPartitioner.partition(gaussGrid);
-    const Mesh structuredGaussMesh = meshGenerator.generate(gaussGrid, gaussToCSDistribution);
+    const grid::MatchingMeshPartitioner CSPartitioner(CSFunctionSpace.mesh(), atlas::option::type("cubedsphere"));
+    const grid::Distribution gaussToCSDistribution(gaussGrid, CSPartitioner);
+    const Mesh structuredGaussMesh = atlas::StructuredMeshGenerator().generate(gaussGrid, gaussToCSDistribution); // Problem occurs here on non-rubix cube ranks.
+}
+
+CASE("CS12 O12") {
+    testFunction("CS-LFR-12", "O12");
+}
+
+CASE("CS12 F12") {
+    testFunction("CS-LFR-12", "F12");
+}
+
+CASE("CS15 O15") {
+    testFunction("CS-LFR-15", "O15");
+}
+
+CASE("CS15 F15") {
+    testFunction("CS-LFR-15", "O15");
+}
+
+CASE("CS12 O8") {
+    testFunction("CS-LFR-12", "O8");
+}
+
+CASE("CS12 F8") {
+    testFunction("CS-LFR-12", "F8");
 }
 
 //-----------------------------------------------------------------------------
